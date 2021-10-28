0ecdaba1584cfd90ed88aba370d2debd9d72f924
ElVerduras <dani.unlp17@gmail.com>
2021-10-28 18:06:34 -0300

    Cambio de nombre de un archivo

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,14 @@
 class UserMailer < ApplicationMailer
-end
+    default from: 'notifications@example.com'
+  
+    def signup_check
+      @user = params[:user]
+      @url  = 'http://example.com/login'
+      mail
+        (
+          to: @user.email, 
+          subject: 'Clave de vacunacion: ' 
+        )
+    end
+  end
+  
