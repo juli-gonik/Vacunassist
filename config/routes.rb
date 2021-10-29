520d3c3c8328fc72866f9fd71735f137e70044a1
Julia Gonik <juligonik@gmail.com>
2021-10-29 16:38:49 -0300

    Configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "main#home"
+  devise_for :user_patients, controllers: {
+    registrations: 'user_patients/registrations',
+    sessions:      'user_patients/sessions'
+  }
+
+  root 'main#home'
+
+  # resources :user_patients
 end
