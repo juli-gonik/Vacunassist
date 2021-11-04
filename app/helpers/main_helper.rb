4752a9faedd7604d5c3bf0b776c724af1df8a86a
Julia Gonik <juligonik@gmail.com>
2021-11-04 08:22:12 -0300

    models

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,31 @@
 module MainHelper
+
+  def translate_status(status)
+    case status
+    when 'pending'
+      'Pendiente'
+    when 'confirmed'
+      'Confirmado'
+    when 'past'
+      'Atendido'
+    end
+  end
+
+  def show_date(appointment)
+    "#{name_of_the_day(appointment)} #{day_complete(appointment)}"
+  end
+
+  def name_of_the_day(appointment)
+    t(appointment.last_dose_date.strftime('%A').downcase.to_sym, scope: [:date])
+  end
+
+  def day_complete(appointment)
+    appointment.last_dose_date.strftime('%d/%m/%Y')
+  end
+
+  def appointment_date(appointment)
+    return 'Pendiente a confirmación' if appointment.pending?
+    return '-' if appointment.past? && (appointment.covid? && appointment.dose.zero? || appointment.last_dose_date.blank?)
+    return show_date(appointment) if appointment.past?
+  end
 end
--- app/models/user_patient.rb
+++ app/models/user_patient.rb
@@ -5,18 +5,13 @@ class UserPatient < User
   validates :dni, uniqueness: true
   validates :dni, numericality: { only_integer: true, greater_than_or_equal_to: 1_000_000, allow_blank: true }
 
-<<<<<<< HEAD
   has_many :appointments, dependent: :destroy
   validates_associated :appointments
-=======
-  has_many :appointments, dependent: :destroy, index_errors: true
 
   accepts_nested_attributes_for :appointments
->>>>>>> commit
 
   belongs_to :vacunatorio
 
-<<<<<<< HEAD
   accepts_nested_attributes_for :appointments
 
   require 'date'
@@ -25,6 +20,4 @@ class UserPatient < User
     now = Time.now.utc.to_date
     now.year - birth_date.year - ((now.month > birth_date.month || (now.month == birth_date.month && now.day >= birth_date.day)) ? 0 : 1)
   end
-=======
->>>>>>> commit
 end
